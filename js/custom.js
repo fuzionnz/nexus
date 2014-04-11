e97add47d27772fe7608491bb941404758f0cb9a
Nesta <nguerrero@emergya.com>
2014-04-12 00:26:17 +0200

    Update info and delete line superfish

--- js/custom.js
+++ js/custom.js
@@ -4,7 +4,6 @@ jQuery(window).load(function() {
 
 	jQuery('#main-menu > ul').superfish({ 
 		delay:       500,								// 0.1 second delay on mouseout 
-		animation:   { opacity:'show',height:'show'},	// fade-in and slide-down animation 
 		dropShadows: true								// disable drop shadows 
 	});	  
 
